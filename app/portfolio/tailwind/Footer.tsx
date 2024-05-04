c8b03a64ca51ea42d95be5bd47aeb06020b03a87
Ahmad Yunus <yunusmujadidi@gmail.com>
2024-05-04 19:33:26 +0700

    add main page

---
+++ app/portfolio/tailwind/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="bg-gray-600"></div>
+    </footer>
+  );
+};
+
+export default Footer;
--- app/portfolio/tailwind/page.tsx
+++ app/portfolio/tailwind/page.tsx
@@ -2,12 +2,14 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Main from "./Main";
+import Footer from "./Footer";
 
 const page = () => {
   return (
     <div className="bg-white min-h-screen">
       <Navbar />
       <Main />
+      <Footer />
     </div>
   );
 };
