494a13d77b3fd2e6dd8b3a4c232afab0a6311978
Ahmad Yunus <yunusmujadidi@gmail.com>
2024-05-14 02:13:40 +0700

    refactor: Update layout component to use flexbox for responsive design

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,13 +23,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn(MainFont.className, PantonFont.variable)}>
-        <Maxwidthwrapper>
-          <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
-            <Navbar />
-            {children}
-          </main>
-        </Maxwidthwrapper>
+      <body
+        className={cn(
+          MainFont.className,
+          PantonFont.variable,
+          "max-w-2xl flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto"
+        )}
+      >
+        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+          <Navbar />
+          {children}
+        </main>
       </body>
     </html>
   );
