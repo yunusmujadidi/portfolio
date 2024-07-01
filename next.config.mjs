e38849acbc88a3d37ebedd12e34bc1458b200b84
Ahmad Yunus <yunusmujadidi@gmail.com>
2024-07-01 23:41:07 +0700

    chore: update next.config to remove unused image domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["tailwindui.com", "wikimedia.org"],
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
